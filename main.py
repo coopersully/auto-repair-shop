341ccb988f2892b976e903bde24bd182295d705e
Cooper Sullivan <thecoopersully@gmail.com>
2023-04-17 10:26:29 -0500

    Begin add, delete, modify, search implementation

--- main.py
+++ main.py
@@ -52,14 +52,10 @@ def connect_db():
     if cnx:
         cursor = cnx.cursor()
         try:
-            cursor.execute(f"USE {DATABASE}")
+            cnx.database = DATABASE
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_BAD_DB_ERROR:
-                if create_database(cnx):
-                    cursor.execute(f"USE {DATABASE}")
-                else:
-                    messagebox.showerror("Error", "Failed to create and connect to the database.")
-                    return None
+                return None
             else:
                 messagebox.showerror("Error", str(err))
                 return None
@@ -68,7 +64,10 @@ def connect_db():
 
 
 # Create tables if they don't exist
-def create_tables(cnx, schema):
+def create_tables(cnx, schema, created_db):
+    if not created_db:
+        return True
+
     cursor = cnx.cursor()
     for statement in schema.split(";"):
         if statement.strip():
@@ -87,13 +86,6 @@ def get_all_records(table_name):
     rows = cursor.fetchall()
     cursor.close()
     return rows
-
-
-def refresh_tree(tree, table_name):
-    tree.delete(*tree.get_children())
-    records = get_all_records(table_name)
-    for row in records:
-        tree.insert("", tk.END, values=row)
 
 
 # Create the tkinter UI and functions here
@@ -116,7 +108,7 @@ def main_window():
     owner_tree.heading("phone", text="Phone")
     owner_tree.heading("email", text="Email")
     owner_tree.pack(fill=tk.BOTH, expand=True)
-
+    add_buttons(owner_tab, owner_tree, "owner", ["first_name", "last_name", "phone", "email"], "owner_id")
     refresh_tree(owner_tree, "owner")
 
     # Cars tab
@@ -130,7 +122,7 @@ def main_window():
     car_tree.heading("year", text="Year")
     car_tree.heading("owner_id", text="Owner ID")
     car_tree.pack(fill=tk.BOTH, expand=True)
-
+    add_buttons(car_tab, car_tree, "car", ["make", "model", "year", "owner_id"], "car_id")
     refresh_tree(car_tree, "car")
 
     # Employees tab
@@ -144,7 +136,7 @@ def main_window():
     employee_tree.heading("last_name", text="Last Name")
     employee_tree.heading("hire_date", text="Hire Date")
     employee_tree.pack(fill=tk.BOTH, expand=True)
-
+    add_buttons(employee_tab, employee_tree, "employee", ["first_name", "last_name", "hire_date"], "employee_id")
     refresh_tree(employee_tree, "employee")
 
     # Services tab
@@ -157,7 +149,7 @@ def main_window():
     service_tree.heading("service_name", text="Service Name")
     service_tree.heading("service_description", text="Service Description")
     service_tree.pack(fill=tk.BOTH, expand=True)
-
+    add_buttons(service_tab, service_tree, "service", ["description", "cost", "car_id", "employee_id"], "service_id")
     refresh_tree(service_tree, "service")
 
     # Repairs tab
@@ -180,9 +172,148 @@ def main_window():
     root.mainloop()
 
 
-# Connect to the database and create tables
-cnx = connect_db()
-if cnx:
-    if create_tables(cnx, schema):
-        main_window()
-    cnx.close()
+def add_record(table_name, columns, values):
+    cursor = cnx.cursor()
+    column_names = ', '.join(columns)
+    placeholders = ', '.join(['%s'] * len(values))
+    query = f"INSERT INTO {table_name} ({column_names}) VALUES ({placeholders})"
+    cursor.execute(query, values)
+    cnx.commit()
+    cursor.close()
+
+
+def update_record(table_name, primary_key_column, primary_key_value, column, new_value):
+    cursor = cnx.cursor()
+    query = f"UPDATE {table_name} SET {column} = %s WHERE {primary_key_column} = %s"
+    cursor.execute(query, (new_value, primary_key_value))
+    cnx.commit()
+    cursor.close()
+
+
+def delete_record(table_name, primary_key_column, primary_key_value):
+    cursor = cnx.cursor()
+    query = f"DELETE FROM {table_name} WHERE {primary_key_column} = %s"
+    cursor.execute(query, (primary_key_value,))
+    cnx.commit()
+    cursor.close()
+
+
+def search_records(table_name, column, value):
+    cursor = cnx.cursor()
+    query = f"SELECT * FROM {table_name} WHERE {column} LIKE %s"
+    cursor.execute(query, (f"%{value}%",))
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
+
+
+def show_add_dialog(table_name, columns, tree):
+    def submit():
+        values = [entry.get() for entry in entries]
+        add_record(table_name, columns, values)
+        refresh_tree(tree, table_name)
+        dialog.destroy()
+
+    dialog = tk.Toplevel()
+    dialog.title(f"Add {table_name.capitalize()} Record")
+    entries = []
+    for i, column in enumerate(columns):
+        label = ttk.Label(dialog, text=column.capitalize())
+        label.grid(row=i, column=0, padx=10, pady=10)
+        entry = ttk.Entry(dialog)
+        entry.grid(row=i, column=1, padx=10, pady=10)
+        entries.append(entry)
+    submit_button = ttk.Button(dialog, text="Submit", command=submit)
+    submit_button.grid(row=len(columns), columnspan=2, pady=10)
+
+
+def refresh_tree(tree, table_name, column=None, search_query=None):
+    tree.delete(*tree.get_children())
+    if column and search_query:
+        records = search_records(table_name, column, search_query)
+    else:
+        records = get_all_records(table_name)
+    for row in records:
+        tree.insert("", tk.END, values=row)
+
+
+# Add buttons for the CRUD operations
+def add_buttons(tab, tree, table_name, columns, primary_key_column):
+    add_button = ttk.Button(tab, text="Add", command=lambda: show_add_dialog(table_name, columns, tree))
+    add_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+    delete_button = ttk.Button(tab, text="Delete",
+                               command=lambda: delete_record(table_name, primary_key_column, tree.selection()[0]))
+    delete_button.pack(side=tk.LEFT, padx=10)
+
+    modify_button = ttk.Button(tab, text="Modify",
+                               command=lambda: show_modify_dialog(tab, table_name, primary_key_column, tree))
+    modify_button.pack(side=tk.LEFT, padx=10)
+
+    search_label = ttk.Label(tab, text="Search:")
+    search_label.pack(side=tk.LEFT, padx=10)
+
+    search_entry = ttk.Entry(tab)
+    search_entry.pack(side=tk.LEFT, padx=10)
+
+    search_button = ttk.Button(tab, text="Search",
+                               command=lambda: refresh_tree(tree, table_name, columns[1], search_entry.get()))
+    search_button.pack(side=tk.LEFT, padx=10)
+
+    clear_search_button = ttk.Button(tab, text="Clear", command=lambda: refresh_tree(tree, table_name))
+    clear_search_button.pack(side=tk.LEFT, padx=10)
+
+
+def show_modify_dialog(parent, table_name, tree, fields, id_field):
+    selected_item = tree.selection()[0]
+    selected_data = tree.item(selected_item)["values"]
+    record_id = selected_data[0]
+
+    top = tk.Toplevel(parent)
+    top.title(f"Modify {table_name.capitalize()} Record")
+
+    labels = []
+    entries = []
+
+    for i, field in enumerate(fields):
+        label = ttk.Label(top, text=f"{field.capitalize()}:")
+        label.grid(row=i, column=0, sticky=tk.W, padx=(10, 5), pady=(10, 0))
+
+        entry = ttk.Entry(top)
+        entry.insert(0, selected_data[i + 1])
+        entry.grid(row=i, column=1, sticky=tk.E, padx=(5, 10), pady=(10, 0))
+
+        labels.append(label)
+        entries.append(entry)
+
+    def modify_record():
+        update_fields = {field: entry.get() for field, entry in zip(fields, entries)}
+        update_record(table_name, id_field, record_id, update_fields)
+        refresh_tree(tree, table_name)
+        top.destroy()
+
+    modify_button = ttk.Button(top, text="Modify", command=modify_record)
+    modify_button.grid(row=len(fields), column=0, columnspan=2, pady=(10, 10))
+
+    top.mainloop()
+
+
+def check_database_exists(cnx, database_name):
+    cursor = cnx.cursor()
+    cursor.execute("SHOW DATABASES")
+    databases = [row[0] for row in cursor.fetchall()]
+    cursor.close()
+
+    return database_name in databases
+
+
+if __name__ == "__main__":
+    # Connect to the database and create tables
+    cnx = connect_db()
+    created_db = False
+    if cnx:
+        if not check_database_exists(cnx, DATABASE):
+            created_db = create_database(cnx)
+        if create_tables(cnx, schema, created_db):
+            main_window()
+        cnx.close()
