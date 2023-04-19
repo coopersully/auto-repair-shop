1bdbc23413782fdc75892ad99be990c6d04f87e5
Cooper Sullivan <thecoopersully@gmail.com>
2023-04-19 16:57:19 -0500

    Begin UI updates

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
-import tkinter as tk
+import customtkinter as ctk
 from tkinter import messagebox, ttk
-
-import mysql.connector
 from dotenv import load_dotenv
+import mysql.connector
 from mysql.connector import errorcode
+import tkinter as tk
 
 load_dotenv()
 
@@ -60,14 +60,14 @@ def get_all_records(table_name):
 
 # Create interface main window, views, and buttons
 def main_window():
-    root = tk.Tk()
+    root = ctk.CTk()
     root.title("Auto Repair Shop Database")
 
     # Create a tabbed interface for each table
     tab_control = ttk.Notebook(root)
 
     # Owners tab
-    owner_tab = ttk.Frame(tab_control)
+    owner_tab = ctk.CTkFrame(tab_control)
     tab_control.add(owner_tab, text="Owners")
 
     owner_tree = ttk.Treeview(owner_tab, columns=("owner_id", "first_name", "last_name", "phone", "email"),
@@ -77,12 +77,12 @@ def main_window():
     owner_tree.heading("last_name", text="Last Name")
     owner_tree.heading("phone", text="Phone")
     owner_tree.heading("email", text="Email")
-    owner_tree.pack(fill=tk.BOTH, expand=True)
+    owner_tree.pack(fill=ctk.BOTH, expand=True)
     add_buttons(owner_tab, owner_tree, "owner", ["first_name", "last_name", "phone", "email"], "owner_id")
     refresh_tree(owner_tree, "owner")
 
     # Cars tab
-    car_tab = ttk.Frame(tab_control)
+    car_tab = ctk.CTkFrame(tab_control)
     tab_control.add(car_tab, text="Cars")
 
     car_tree = ttk.Treeview(car_tab, columns=("car_id", "make", "model", "year", "owner_id"), show="headings")
@@ -91,12 +91,12 @@ def main_window():
     car_tree.heading("model", text="Model")
     car_tree.heading("year", text="Year")
     car_tree.heading("owner_id", text="Owner ID")
-    car_tree.pack(fill=tk.BOTH, expand=True)
+    car_tree.pack(fill=ctk.BOTH, expand=True)
     add_buttons(car_tab, car_tree, "car", ["make", "model", "year", "owner_id"], "car_id")
     refresh_tree(car_tree, "car")
 
     # Employees tab
-    employee_tab = ttk.Frame(tab_control)
+    employee_tab = ctk.CTkFrame(tab_control)
     tab_control.add(employee_tab, text="Employees")
 
     employee_tree = ttk.Treeview(employee_tab, columns=("employee_id", "first_name", "last_name", "hire_date"),
@@ -105,12 +105,12 @@ def main_window():
     employee_tree.heading("first_name", text="First Name")
     employee_tree.heading("last_name", text="Last Name")
     employee_tree.heading("hire_date", text="Hire Date")
-    employee_tree.pack(fill=tk.BOTH, expand=True)
+    employee_tree.pack(fill=ctk.BOTH, expand=True)
     add_buttons(employee_tab, employee_tree, "employee", ["first_name", "last_name", "hire_date"], "employee_id")
     refresh_tree(employee_tree, "employee")
 
     # Services tab
-    service_tab = ttk.Frame(tab_control)
+    service_tab = ctk.CTkFrame(tab_control)
     tab_control.add(service_tab, text="Services")
 
     service_tree = ttk.Treeview(service_tab, columns=("service_id", "service_name", "service_description"),
@@ -118,12 +118,12 @@ def main_window():
     service_tree.heading("service_id", text="Service ID")
     service_tree.heading("service_name", text="Service Name")
     service_tree.heading("service_description", text="Service Description")
-    service_tree.pack(fill=tk.BOTH, expand=True)
+    service_tree.pack(fill=ctk.BOTH, expand=True)
     add_buttons(service_tab, service_tree, "service", ["description", "cost", "car_id", "employee_id"], "service_id")
     refresh_tree(service_tree, "service")
 
     # Repairs tab
-    repair_tab = ttk.Frame(tab_control)
+    repair_tab = ctk.CTkFrame(tab_control)
     tab_control.add(repair_tab, text="Repairs")
 
     repair_tree = ttk.Treeview(repair_tab, columns=("repair_id", "car_id", "service_id", "employee_id", "repair_date"),
@@ -133,11 +133,11 @@ def main_window():
     repair_tree.heading("service_id", text="Service ID")
     repair_tree.heading("employee_id", text="Employee ID")
     repair_tree.heading("repair_date", text="Repair Date")
-    repair_tree.pack(fill=tk.BOTH, expand=True)
+    repair_tree.pack(fill=ctk.BOTH, expand=True)
 
     refresh_tree(repair_tree, "repair")
 
-    tab_control.pack(fill=tk.BOTH, expand=True)
+    tab_control.pack(fill=ctk.BOTH, expand=True)
 
     root.mainloop()
 
@@ -193,12 +193,12 @@ def show_add_dialog(table_name, columns, tree):
     dialog.title(f"Add {table_name.capitalize()} Record")
     entries = []
     for i, column in enumerate(columns):
-        label = ttk.Label(dialog, text=column.capitalize())
+        label = ctk.CTkLabel(dialog, text=column.capitalize())
         label.grid(row=i, column=0, padx=10, pady=10)
-        entry = ttk.Entry(dialog)
+        entry = ctk.CTkEntry(dialog)
         entry.grid(row=i, column=1, padx=10, pady=10)
         entries.append(entry)
-    submit_button = ttk.Button(dialog, text="Submit", command=submit)
+    submit_button = ctk.CTkButton(dialog, text="Submit", command=submit)
     submit_button.grid(row=len(columns), columnspan=2, pady=10)
 
 
@@ -207,23 +207,23 @@ def refresh_tree(tree, table_name):
     tree.delete(*tree.get_children())
     records = get_all_records(table_name)
     for row in records:
-        tree.insert("", tk.END, values=row)
+        tree.insert("", ctk.END, values=row)
 
 
 # Add buttons for the CRUD operations
 def add_buttons(tab, tree, table_name, columns, primary_key_column):
-    add_button = ttk.Button(tab, text="Add", command=lambda: show_add_dialog(table_name, columns, tree))
-    add_button.pack(side=tk.LEFT, padx=10, pady=10)
+    add_button = ctk.CTkButton(tab, text="Add", command=lambda: show_add_dialog(table_name, columns, tree))
+    add_button.pack(side=ctk.LEFT, padx=10, pady=10)
 
-    delete_button = ttk.Button(tab, text="Delete",
+    delete_button = ctk.CTkButton(tab, text="Delete",
                                command=lambda: delete_record(table_name, primary_key_column,
                                                              tree.item(tree.selection()[0])['values'][0], tree))
-    delete_button.pack(side=tk.LEFT, padx=10)
+    delete_button.pack(side=ctk.LEFT, padx=10)
 
-    modify_button = ttk.Button(tab, text="Modify",
+    modify_button = ctk.CTkButton(tab, text="Modify",
                                command=lambda: show_modify_dialog(tab, table_name, primary_key_column,
                                                                   tree.item(tree.selection()[0])['values'][0], tree))
-    modify_button.pack(side=tk.LEFT, padx=10)
+    modify_button.pack(side=ctk.LEFT, padx=10)
 
 
 # Pop-up dialog for editing the selected entry
@@ -232,7 +232,7 @@ def show_modify_dialog(parent, table_name, primary_key_column, id_field, tree):
     selected_data = tree.item(selected_item)["values"]
     record_id = selected_data[0]
 
-    top = tk.Toplevel(parent)
+    top = ctk.CTkToplevel(parent)
     top.title(f"Modify {table_name.capitalize()} Record")
 
     curr = conn.cursor()
@@ -244,12 +244,12 @@ def show_modify_dialog(parent, table_name, primary_key_column, id_field, tree):
     entries = []
 
     for i, field in enumerate(fields):
-        label = ttk.Label(top, text=f"{field.capitalize()}:")
-        label.grid(row=i, column=0, sticky=tk.W, padx=(10, 5), pady=(10, 0))
+        label = ctk.CTkLabel(top, text=f"{field.capitalize()}:")
+        label.grid(row=i, column=0, sticky=ctk.W, padx=(10, 5), pady=(10, 0))
 
-        entry = ttk.Entry(top)
+        entry = ctk.CTkEntry(top)
         entry.insert(0, selected_data[i + 1])
-        entry.grid(row=i, column=1, sticky=tk.E, padx=(5, 10), pady=(10, 0))
+        entry.grid(row=i, column=1, sticky=ctk.E, padx=(5, 10), pady=(10, 0))
 
         labels.append(label)
         entries.append(entry)
@@ -260,7 +260,7 @@ def show_modify_dialog(parent, table_name, primary_key_column, id_field, tree):
         refresh_tree(tree, table_name)
         top.destroy()
 
-    modify_button = ttk.Button(top, text="Modify", command=modify_record)
+    modify_button = ctk.CTkButton(top, text="Modify", command=modify_record)
     modify_button.grid(row=len(fields), column=0, columnspan=2, pady=(10, 10))
 
     top.mainloop()
