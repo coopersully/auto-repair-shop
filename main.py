f4a49709c10a56397cd2b33716068957cb5b3a25
Cooper Sullivan <thecoopersully@gmail.com>
2023-04-19 16:32:50 -0500

    Organize code

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import mysql.connector
-import sqlparse
-from dotenv import load_dotenv
-from mysql.connector import errorcode
+import os
 import tkinter as tk
 from tkinter import messagebox, ttk
-import os
+
+import mysql.connector
+from dotenv import load_dotenv
+from mysql.connector import errorcode
 
 load_dotenv()
 
@@ -15,19 +15,15 @@ HOST = os.environ.get("HOST")
 DATABASE = os.environ.get("DATABASE")
 PORT = os.environ.get("PORT")
 
-# Read schema.sql file
-with open("schema.sql", "r") as f:
-    schema = f.read()
 
-
-# Establish a connection to the server
+# Establish a connection to the MySQL server
 def connect_server():
     try:
-        print(f'Logging in as {USERNAME} with {PASSWORD}...')
-        cnx = mysql.connector.connect(
+        print(f'Logging in as user "{USERNAME}" with password "{PASSWORD}"...')
+        conn = mysql.connector.connect(
             user=USERNAME, password=PASSWORD, host=HOST
         )
-        return cnx
+        return conn
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             messagebox.showerror("Error", "Access denied. Check your username and password.")
@@ -38,30 +34,31 @@ def connect_server():
 
 # Establish a connection to the database
 def connect_db():
-    cnx = connect_server()
-    if cnx:
-        cursor = cnx.cursor()
+    conn = connect_server()
+    if conn:
+        curr = conn.cursor()
         try:
-            cnx.database = DATABASE
+            conn.database = DATABASE
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_BAD_DB_ERROR:
                 return None
             else:
                 messagebox.showerror("Error", str(err))
                 return None
-        cursor.close()
-    return cnx
+        curr.close()
+    return conn
 
 
+# Get all entries in a given table
 def get_all_records(table_name):
-    cursor = cnx.cursor()
-    cursor.execute(f"SELECT * FROM {table_name}")
-    rows = cursor.fetchall()
-    cursor.close()
+    curr = conn.cursor()
+    curr.execute(f"SELECT * FROM {table_name}")
+    rows = curr.fetchall()
+    curr.close()
     return rows
 
 
-# Create the tkinter UI and functions here
+# Create interface main window, views, and buttons
 def main_window():
     root = tk.Tk()
     root.title("Auto Repair Shop Database")
@@ -145,31 +142,34 @@ def main_window():
     root.mainloop()
 
 
+# Take fields from the add pop-up dialog and create a new entry
 def add_record(table_name, columns, values):
-    cursor = cnx.cursor()
+    curr = conn.cursor()
     column_names = ', '.join(columns)
     placeholders = ', '.join(['%s'] * len(values))
     query = f"INSERT INTO {table_name} ({column_names}) VALUES ({placeholders})"
-    cursor.execute(query, values)
-    cnx.commit()
-    cursor.close()
+    curr.execute(query, values)
+    conn.commit()
+    curr.close()
 
 
+# Take fields from the modify pop-up dialog and update the given entry
 def update_record(table_name, id_field, record_id, update_fields):
     set_clause = ', '.join([f"{field} = %s" for field in update_fields])
     query = f"UPDATE {table_name} SET {set_clause} WHERE {id_field} = %s"
 
-    cursor = cnx.cursor()
-    cursor.execute(query, list(update_fields.values()) + [record_id])
-    cnx.commit()
+    curr = conn.cursor()
+    curr.execute(query, list(update_fields.values()) + [record_id])
+    conn.commit()
 
 
+# Delete the selected entry
 def delete_record(table_name, primary_key_column, primary_key_value, tree):
     query = f"DELETE FROM {table_name} WHERE {primary_key_column} = %s"
     try:
-        cursor = cnx.cursor()
-        cursor.execute(query, (primary_key_value,))
-        cnx.commit()
+        curr = conn.cursor()
+        curr.execute(query, (primary_key_value,))
+        conn.commit()
         refresh_tree(tree, table_name)
     except mysql.connector.errors.IntegrityError as e:
         if e.errno == 1451:
@@ -181,6 +181,7 @@ def delete_record(table_name, primary_key_column, primary_key_value, tree):
         messagebox.showerror("Error", f"An error occurred while deleting the record: {e}")
 
 
+# Pop-up dialog for adding a new entry
 def show_add_dialog(table_name, columns, tree):
     def submit():
         values = [entry.get() for entry in entries]
@@ -201,6 +202,7 @@ def show_add_dialog(table_name, columns, tree):
     submit_button.grid(row=len(columns), columnspan=2, pady=10)
 
 
+# Update data in the current tree view
 def refresh_tree(tree, table_name):
     tree.delete(*tree.get_children())
     records = get_all_records(table_name)
@@ -224,6 +226,7 @@ def add_buttons(tab, tree, table_name, columns, primary_key_column):
     modify_button.pack(side=tk.LEFT, padx=10)
 
 
+# Pop-up dialog for editing the selected entry
 def show_modify_dialog(parent, table_name, primary_key_column, id_field, tree):
     selected_item = tree.selection()[0]
     selected_data = tree.item(selected_item)["values"]
@@ -232,9 +235,9 @@ def show_modify_dialog(parent, table_name, primary_key_column, id_field, tree):
     top = tk.Toplevel(parent)
     top.title(f"Modify {table_name.capitalize()} Record")
 
-    cursor = cnx.cursor()
-    cursor.execute(f"DESCRIBE {table_name}")
-    column_data = cursor.fetchall()
+    curr = conn.cursor()
+    curr.execute(f"DESCRIBE {table_name}")
+    column_data = curr.fetchall()
     fields = [column[0] for column in column_data if column[0] != primary_key_column]
 
     labels = []
@@ -264,7 +267,7 @@ def show_modify_dialog(parent, table_name, primary_key_column, id_field, tree):
 
 
 if __name__ == "__main__":
-    cnx = connect_db()
-    if cnx:
+    conn = connect_db()
+    if conn:
         main_window()
-        cnx.close()
+        conn.close()
